118627013bb46078455845398a215a2f10a51933
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2024-07-09 15:53:36 +0530

    UPDATE: axios insted of fetch and flag classname removed from img

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = ({ country }) => (
   <div className="countryCard">
-    <img src={country.flags.png} alt={`Flag of ${country.name.common}`} className="flag" />
+    <img src={country.flags.png} alt={`Flag of ${country.name.common}`}/>
     <p>{country.name.common}</p>
   </div>
 );
